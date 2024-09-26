30abda788fd46dbcb1593ec966f1f7a98eee8c44
mambo-dev <mambo.michael.22>
2024-09-26 20:37:11 +0300

    feat:skeleton project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+</script>
+
+<div class="grid w-full max-w-sm items-center gap-1.5">
+	<Label for="picture">Picture</Label>
+	<Input id="picture" type="file" />
+</div>
